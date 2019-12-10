03f70d86abac661654c5434bef65a2e2058fa5af
iifred <594703382@qq.com>
2019-12-10 16:55:41 +0800

    1：提交代码

---
+++ src/main/java/com/springboot/elastic/ElasticJestDatasearchApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.elastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticJestDatasearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticJestDatasearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/elastic/ElasticJestDatasearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.elastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticJestDatasearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
