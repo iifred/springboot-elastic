1691c6d4c39b861e2ef903a66684a92d867d6e6a
iifred <594703382@qq.com>
2019-12-10 18:00:52 +0800

    1：elasticsearch配置

--- src/main/java/com/springboot/elastic/ElasticJestDatasearchApplication.java
+++ src/main/java/com/springboot/elastic/ElasticJestDatasearchApplication.java
@@ -3,6 +3,11 @@ package com.springboot.elastic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *  docker 安装容器命令
+ *   注意springboot和elasticsearch的版本关系 可以springboot官网查询
+ *   docker run -it -e ES_JAVA_OPTS="-Xms256m -Xmx256m"  -e "discovery.type=single-node" -p 9200:9200 -p  9300:9300 --name ES01 911f580307ae
+ */
 @SpringBootApplication
 public class ElasticJestDatasearchApplication {
 
